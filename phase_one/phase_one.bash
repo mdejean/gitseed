666715236865e147491dd1a4fca5e1dd3e71a993
Paul Dejean <paul@pauldejean.com>
2018-10-24 20:17:02 +0000

    Time to test phase_one now.

--- phase_one/phase_one.bash
+++ phase_one/phase_one.bash
@@ -1,2 +1,21 @@
 #!/bin/bash
-echo "Phase one has been successfully achieved."
+set -e
+
+echo "#### Unpacking the repository ####"
+mkdir ~/gitseed
+tar -C ~/gitseed -zxf /tmp/gitseed.tar.gz
+rm /tmp/gitseed.tar.gz
+
+echo "#### Downloading and installing gotty ####"
+gotty_url="https://github.com/yudai/gotty/releases/download/v2.0.0-alpha.3/gotty_2.0.0-alpha.3_linux_amd64.tar.gz"
+wget --progress=dot:giga -O /tmp/gotty.tar.gz "$gotty_url"
+tar -zxvf /tmp/gotty.tar.gz
+sudo cp /tmp/gotty /usr/local/bin/gotty
+rm /tmp/gotty.tar.gz
+
+echo "#### Executing phase_two in gotty ####"
+sudo /usr/local/bin/gotty -p 80 --timeout 60 ~/gitseed/phase_two/phase_two.bash &> /tmp/gotty.log &
+disown -h
+echo "The gotty log is at /tmp/gotty.log"
+echo "Visit http://$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google") to continue!"
+
