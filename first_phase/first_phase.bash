3552c91cbf55cfb2d04b2e81c82b3c0a888d2eb7
Paul Dejean <paul@pauldejean.com>
2018-10-24 20:40:34 +0000

    Can we get to second phase?

--- first_phase/first_phase.bash
+++ first_phase/first_phase.bash
@@ -14,8 +14,7 @@ sudo cp /tmp/gotty /usr/local/bin/gotty
 rm /tmp/gotty.tar.gz
 
 echo "#### Executing phase_two in gotty ####"
-sudo /usr/local/bin/gotty -p 80 --timeout 60 ~/gitseed/phase_two/phase_two.bash &> /tmp/gotty.log &
+sudo /usr/local/bin/gotty -p 80 --timeout 60 ~/gitseed/second_phase/second_phase.bash &> /tmp/gotty.log &
 disown -h
 echo "The gotty log is at /tmp/gotty.log"
-echo "Visit http://$(curl http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google") to continue!"
-
+echo "Visit http://$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google") to continue!"
