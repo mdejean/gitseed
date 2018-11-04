8f375a19d3092a9f48f2972224ea2ca6a09780d8
Paul Dejean <paul@pauldejean.com>
2018-11-03 20:31:25 -0500

    What's saltstack???

--- second_phase/README.md
+++ second_phase/README.md
@@ -5,3 +5,9 @@ However our bastion instance is currently very weak.
 First we will install ansible.
 
 Then we run an ansible playbook on it, allowing us to provision it however we want.
+
+TODO: Current musings.
+
+Lets try using ansible to bootstrap saltstack.
+
+Is saltstack more powerful? I'm not sure, but I need to learn it for my new job.
--- second_phase/second_phase.bash
+++ second_phase/second_phase.bash
@@ -1,2 +1,15 @@
 #!/bin/bash
-echo "Second phase successful!!!"
+set -e
+cd "${0%/*}"
+
+echo "### Installing python-pip ###"
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install -y python-pip
+
+echo "### Installing ansible ###"
+sudo pip install ansible
+
+echo "### Running the local ansible playbook! ###"
+export ANSIBLE_RETRY_FILES_ENABLED=False
+ansible-playbook bastion.yml
